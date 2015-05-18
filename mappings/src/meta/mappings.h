48cc1aae041086f4bbd0eb0c38f169a06e8a456d
Tudor Berariu <tudor.berariu@gmail.com>
2015-05-18 22:27:21 +0300

    Added source files to write magic in

---
+++ mappings/src/meta/mappings.h
@@ -0,0 +1,8 @@
+#ifndef MAPPINGS_H
+#define MAPPINGS_H
+
+template<size_t in, size_t out>
+struct Mapping{
+
+};
+#endif
---
+++ mappings/src/test-mappings.cc
@@ -0,0 +1,12 @@
+// Copyright (C) 2015 Tudor Berariu
+
+#include <iostream>
+#include "meta/mappings.h"
+
+int main(int, char * argv[])
+{
+  std::cout << "Hello, there! Good news: "
+            << argv[0]+2 << " works!"
+            << std::endl;
+  return 0;
+}
